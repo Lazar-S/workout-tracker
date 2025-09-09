10ab5cea6ede2a4f8d9b6b03fda66068edf6a6b9
Lazar S <lazar.stankovic.dev@gmail.com>
2025-09-10 01:25:07 +0200

    routes name change

--- routes/web.php
+++ routes/web.php
@@ -85,8 +85,8 @@ Route::get('/search', [UserController::class, 'search'])->middleware('auth:sanct
 Route::post("/api/login", [AuthController::class, "login"]);
 
 Route::get("/api/routines", [AuthController::class, "getRoutines"])->middleware('auth:sanctum');
-Route::post("/api/routine", [AuthController::class, "createRoutine"])->middleware('auth:sanctum');
-Route::patch("/api/routine", [AuthController::class, "updateRoutine"])->middleware('auth:sanctum');
-Route::delete("/api/routine", [AuthController::class, "deleteRoutine"])->middleware('auth:sanctum');
+Route::post("/api/routines", [AuthController::class, "createRoutine"])->middleware('auth:sanctum');
+Route::patch("/api/routines", [AuthController::class, "updateRoutine"])->middleware('auth:sanctum');
+Route::delete("/api/routines", [AuthController::class, "deleteRoutine"])->middleware('auth:sanctum');
 
 Route::patch("/api/make-public", [AuthController::class, "makePublic"])->middleware('auth:sanctum');
